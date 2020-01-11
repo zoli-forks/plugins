6564b881afb2b3d33e603b438d03eabd8f9191a7
shellscape <andrew@shellscape.org>
2020-01-10 21:43:57 -0500

    chore: add repro

--- packages/pluginutils/rollup.config.js
+++ packages/pluginutils/rollup.config.js
@@ -1,5 +1,5 @@
-import typescript from 'rollup-plugin-typescript';
-import resolve from 'rollup-plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 import pkg from './package.json';
